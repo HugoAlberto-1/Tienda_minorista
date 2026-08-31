bcd4d19e328b9778826509be2a5c23badbfd3447
HugoAlberto-1 <healberto@uca.edu.sv>
2026-08-30 20:37:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -446,7 +446,22 @@ def menu_principal():
 
                         st.session_state["macro_modulo"] = "reportes"
                         st.rerun()
+                with col5:
 
+                    mostrar_macro_tarjeta(
+                        "📊",
+                        "Transacciones",
+                        "Compra y vende productos"
+                    )
+
+                    if st.button(
+                        "📈 Ver reportes",
+                        key="btn_reportes_admin",
+                        use_container_width=True
+                    ):
+
+                        st.session_state["macro_modulo"] = "transacciones"
+                        st.rerun()
             
             # ========================================================
             # REPORTES ADMINISTRADOR
